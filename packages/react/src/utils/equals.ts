6d7a50b449f423d6e0f478bdb9387cc043a357f1
천진아 <jncheon@carmerce.co.kr>
2025-11-18 01:14:10 +0900

    feat: shallowEquals, deepEquals 구현

--- packages/react/src/utils/equals.ts
+++ packages/react/src/utils/equals.ts
@@ -5,7 +5,26 @@
 export const shallowEquals = (a: unknown, b: unknown): boolean => {
   // 여기를 구현하세요.
   // Object.is(), Array.isArray(), Object.keys() 등을 활용하여 1단계 깊이의 비교를 구현합니다.
-  return a === b;
+  if (a === b) return true;
+  if (typeof a !== typeof b) return false;
+
+  // 배열 비교
+  if (typeof a === "object" && Array.isArray(a) && typeof b === "object" && Array.isArray(b)) {
+    if (a.length !== b.length) return false;
+    if (a.some((value, index) => value !== b[index])) return false;
+    return true;
+  }
+
+  //객체 비교
+  if (typeof a === "object" && a !== null && typeof b === "object" && b !== null) {
+    if (Object.keys(a).length !== Object.keys(b).length) return false;
+    if (Object.keys(a).some((key) => a[key as keyof typeof a] !== b[key as keyof typeof b])) return false;
+    return true;
+  }
+
+  // 함수 비교
+  if (typeof a === "function" && typeof b === "function") return a.toString() === b.toString();
+  return Object.is(a, b);
 };
 
 /**
@@ -15,5 +34,18 @@ export const shallowEquals = (a: unknown, b: unknown): boolean => {
 export const deepEquals = (a: unknown, b: unknown): boolean => {
   // 여기를 구현하세요.
   // 재귀적으로 deepEquals를 호출하여 중첩된 구조를 비교해야 합니다.
-  return a === b;
+  if (typeof a !== typeof b) return false;
+  if (typeof a === "object" && Array.isArray(a) && typeof b === "object" && Array.isArray(b)) {
+    if (a.length !== b.length) return false;
+    if (a.some((value, index) => !deepEquals(value, b[index]))) return false;
+    return true;
+  }
+
+  if (typeof a === "object" && a !== null && typeof b === "object" && b !== null) {
+    if (Object.keys(a).length !== Object.keys(b).length) return false;
+    if (Object.keys(a).some((key) => !deepEquals(a[key as keyof typeof a], b[key as keyof typeof b]))) return false;
+    return true;
+  }
+  if (typeof a === "function" && typeof b === "function") return a.toString() === b.toString();
+  return Object.is(a, b);
 };
